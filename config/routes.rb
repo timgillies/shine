126c33dc39ed5daa3236d68d46c11a97267d3b04
Tim Gillies <timothy.gillies@colorado.edu>
2016-08-17 10:06:12 -0600

    updated customer index page with google style results

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   root 'dashboard#index'
 
+  resources :customers, only: [ :index ]
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
